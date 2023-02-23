02dc89a5b80662758540b746ea8213bc3d11ff6b
Matthew Carrell <matthewcarrell@my.unt.edu>
2023-02-22 19:20:23 -0600

    Initilization

---
+++ start.py
@@ -0,0 +1,24 @@
+import torch
+import scipy as sp
+import pandas as pd
+import tensorboardX
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+#load dats
+
+#preprocess data
+
+#split train test val
+
+#define net
+
+#define loss/optimizer
+
+#do training
+
+#save model
+
+#do testing
+
+#save results
