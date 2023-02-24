b4dbca47e4f020bf03ff5b71a76e137bbdb9685c
Sazoji <matthewcarrell@my.unt.edu>
2023-02-24 03:59:45 -0600

    Update start.py

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
 import torch
-import scipy as sp
+import numpy as np
 import pandas as pd
 import tensorboardX
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 #load dats
+dataset = pd.read_csv('covid_data.csv')
 
 #preprocess data
 
